1a0677f2e0789cab895c868a326b9152ba908881
常鸣 <changming.zy@alibaba-inc.com>
2016-11-04 22:03:26 +0800

    配置sae部署

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = koa();
 
 app.use(function *(){
   var path = this.path;
-  this.body = path;
+  this.body = `卖零食的小姑凉 ${path}`;
 });
 
-app.listen(80);
+app.listen(process.env.PORT || 3000);
